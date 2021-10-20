f7eb991dff41c821be59dc482e006fb4085bb594
balthazarely <balthazar.elyj@gmail.com>
2021-10-20 11:46:49 -0600

    fixed links

--- src/components/Spinner/SpinnerBtn.tsx
+++ src/components/Spinner/SpinnerBtn.tsx
@@ -10,7 +10,7 @@ export const SpinnerBtn = ({ nextButtonClick }: NextButtonProps) => {
       onClick={nextButtonClick}
       className=" bg-tonic-base hover:bg-tonic-baseDark  text-white px-4 py-2  cursor-pointer text-sm h-12 flex items-center justify-center   w-full "
     >
-      WHO'S UP NEXT?
+      WHO&apos;S UP NEXT?
     </div>
   );
 };
